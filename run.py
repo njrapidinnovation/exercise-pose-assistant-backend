b5f3710b80742b841fc9c3e06f6c3e4ccce86171
rapidkomal <104576390+rapidkomal@users.noreply.github.com>
2024-07-17 16:43:49 +0530

    commented test function for index

--- run.py
+++ run.py
@@ -18,9 +18,9 @@ flask_app = flask_create_app()
 socketio = SocketIO(flask_app, cors_allowed_origins="*")
 
 
-@flask_app.route("/video", methods=["GET"])
-def test():
-    return render_template("socket_index.html", socket_url=SOCKET_URL)
+# @flask_app.route("/video", methods=["GET"])
+# def test():
+#     return render_template("socket_index.html", socket_url=SOCKET_URL)
 
 
 @flask_app.route("/", methods=["GET"])
@@ -41,7 +41,6 @@ def handle_frame(data):
     if not streaming_state.streaming:
         return
     try:
-        # Measure input size
         input_size = sys.getsizeof(data)
         print(f"Input data size: {input_size} bytes")
 
@@ -52,20 +51,17 @@ def handle_frame(data):
         frame.pts = int(time.time() * 1000000)
         frame.time_base = Fraction(1, 1000000)
 
-        # Measure size before processing
         pre_process_size = sys.getsizeof(frame.to_ndarray())
         print(f"Pre-processed frame size: {pre_process_size} bytes")
 
         processed_frame = process_frame(frame)
 
-        # Measure size after processing
         post_process_size = sys.getsizeof(processed_frame.to_ndarray())
         print(f"Post-processed frame size: {post_process_size} bytes")
 
         _, buffer = cv2.imencode(".jpg", processed_frame.to_ndarray(format="bgr24"))
         processed_image = base64.b64encode(buffer).decode("utf-8")
 
-        # Measure output size
         output_size = sys.getsizeof(processed_image)
         print(f"Output data size: {output_size} bytes")
 
